2d3fd53d80fd9504ec56d27762a4a95dd7a30b74
Nichole McGrew <nicholemcgrew@gmail.com>
2024-06-22 12:08:49 -0500

    api fetch with useEffect

--- component-lifecyle/src/App.jsx
+++ component-lifecyle/src/App.jsx
@@ -1,20 +1,71 @@
-import { useState } from "react"
-import { Child } from "./components/Child"
+import { useEffect, useState } from "react"
+// import { Child } from "./components/Child"
 
-function App() {
-	const [show, setShow] = useState(true)
+export default function App() {
+	const [users, setUsers] = useState()
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState()
 
-	const childComponent = show ? <Child /> : null
+	useEffect(() => {
+		setLoading(true)
+		setError(undefined)
+		const controller = new AbortController()
+		fetch("https://jsonplaceholder.typicode.com/users", {
+			signal: controller.signal,
+		})
+			.then((res) => {
+				if (res.status === 200) {
+					return res.json()
+				} else {
+					return Promise.reject(res)
+				}
+			})
+			.then((data) => {
+				setUsers(data)
+			})
+			.catch((e) => {
+				if (e?.name === "AbortError") return
+				setError(e)
+			})
+			.finally(() => {
+				setLoading(false)
+			})
+		return () => {
+			controller.abort()
+		}
+	}, [])
+
+	let jsx
+	if (loading) {
+		jsx = <h2>Loading...</h2>
+	} else if (error != null) {
+		jsx = <h2>Error!</h2>
+	} else {
+		jsx = JSON.stringify(users)
+	}
 
 	return (
 		<div>
-			<button onClick={() => setShow((currentShow) => !currentShow)}>
-				Show/Hide
-			</button>
-			{childComponent}
+			<h1>Users</h1>
+			{jsx}
 		</div>
 	)
 }
+
+//Show/hide
+// 	const [show, setShow] = useState(true)
+
+// 	const childComponent = show ? <Child /> : null
+
+// 	return (
+// 		<div>
+// 			<button onClick={() => setShow((currentShow) => !currentShow)}>
+// 				Show/Hide
+// 			</button>
+// 			{childComponent}
+// 		</div>
+// 	)
+// }
 
 // NAME AND AGE
 
@@ -51,5 +102,3 @@ function App() {
 // 	</div>
 // )
 // }
-
-export default App
