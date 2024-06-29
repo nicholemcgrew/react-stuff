47f16f9977e08328d8bf225ebce34fd27bc96688
Nichole McGrew <nicholemcgrew@gmail.com>
2024-06-29 06:46:37 -0500

    Typescript

---
+++ typescript/src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      function printName(name: string, name2: string) {
+        console.log(name, name2)
+      }
+    </div>
+  )
+}
+
+export default App
