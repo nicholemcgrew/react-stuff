65529e97efa04a5ece19b0993239ea913439b517
Nichole McGrew <nicholemcgrew@gmail.com>
2024-06-29 07:58:56 -0500

    generics

---
+++ typescript/src/script.ts
@@ -0,0 +1,153 @@
+// const SKILL_LEVELS = ['Beginner', 'Intermediate', "Expert"] as const
+
+// type Person = {
+//     skillLevel: (typeof SKILL_LEVELS)[number]
+// }
+
+// SKILL_LEVELS.forEach(skillLevel => {
+//     console.log(skillLevel)
+// })
+
+// type Person = {
+//     name: String
+//     skillLevel: "Beginner" | "Intermediate" | "Expert"
+// }
+
+// const preson: Person = { name: "Kyle", skillLevel: "Expert"}
+// printSkillLevel(person.skillLevel)
+
+// function printSkillLevel(skillLevel: Person["skillLevel"]) {
+//     console.log(skillLevel)
+// }
+
+// type PeopleGroupedBySkillLevel = {
+//     [index in Person["skillLevel"]]: Person[]
+// }
+
+// type PeopleGroupedBySkillLevel = {
+//     [index: Person["name"]]: Person[]
+// }
+// const a: PeopleGroupedBySkillLevel = {
+//     "Beginner": [{name: "kyle", skillLevel: "Beginner"}]
+// }
+
+// const a = ['sddf', 'd', true]
+
+// type A = (typeof a)[number]
+
+// const b = {
+//     name: "Kyle",
+//     age: 28,
+//     isProgrammer: true,
+// }
+
+// type B = (typeof b)[keyof typeof b]
+
+// let a = 1
+// const b = 1
+
+// const nums = ['1', '2', '3']
+// const a = nums[0]
+// nums.push('sdhsj')
+
+// type Person = {
+//     skillLevel:
+// }
+
+// const person = {
+//     name: 'Kyle',
+//     age: 28,
+//     address: {
+//         streat: "Main St",
+//     },
+// }
+
+// type Tuple = [String, boolean]
+// const a: Tuple = ['sdjks', true]
+
+// Object.entries(person).forEach(([key, value]) => {
+//     console.log(key, value)
+// })
+
+// const input = document.querySelector<HTMLInputElement>(".input")
+
+// console.log(input?.value)
+
+// function getSecond<ArrayType>(array: ArrayType[]) {
+//     return array[1]
+// }
+
+// const a = [1, 2, 3]
+// const b = ['dsjkaf', 'ahdfjah', 'dsahkj']
+
+// const retA = getSecond(a)
+// const retB = getSecond(b)
+
+// const a = new Set<string>()
+// const a = new Map<string, number>()
+
+// a.set('fsdaf', 4)
+
+// type APIResponse<TData> = {
+//     data: TData
+//     isError: boolean
+// }
+
+// type UserResponse = APIResponse<{name: string, age: number}>
+// type BlogResponse = APIResponse<{title: string}>
+
+// const a: APIResponse = {
+//     data: {
+//         name: 'dakjh',
+//         age: 34,
+//     }
+//     isError: false,
+// }
+
+// const b: BlogResponse = {
+//     data: {
+//         title: 'dsaflk'
+//     }
+//     isError: false
+// }
+
+// type APIResponse<TData extends object> = {
+//     data: TData
+//     isError: boolean
+// }
+
+// const a: APIResponse<{name: string}> = {
+//     data: {
+//         name: 'sdhkj'
+//     },
+//     isError: false
+// }
+
+// type APIResponse<TData> = {
+//     data: TData
+//     isError: boolean
+// }
+
+// const a: APIResponse<Array<number>> = {
+//     data: [1, 2, 3],
+//     isError: false,
+// }
+
+function aToO<T>(array: [string, T][]) {
+    const obj: { 
+        [index: string]: T}
+
+    array.forEach(([key, value]) => {
+        obj[key] = value
+    })
+
+    return obj
+}
+
+const arr: [string, number | boolean][] = [
+	["keyOne", 1],
+	["keyTwo", 2],
+	["keyTwo", true],
+]
+
+const obj = aToO(arr)
